02059b1537ace92ff3fdd886b70008001c7b8dff
aaronmccormack15533 <maron.accormack@gmail.com>
2018-10-12 10:41:31 +0100

    White pawn starting move take fix 1

--- ChessProject.java
+++ ChessProject.java
@@ -711,11 +711,9 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
 				}
 				else if((yMovement == 1) && (startY < landingY) && (xMovement == 0)){
 					if(!piecePresent(e.getX(), e.getY())){
-						if(checkWhiteOponent(e.getX(), e.getY())){
-							validMove = true;
-							if(startY == 6){
-								WhiteSuccess = true;
-							}
+						validMove = true;
+						if(startY == 6){
+							WhiteSuccess = true;
 						}
 					}
 				}
